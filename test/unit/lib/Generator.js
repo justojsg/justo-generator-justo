26e76305932d113c7ddee285ed048a524d36a2fe
Raúl G. González <raulggonzalez@justojs.org>
2016-02-22 10:14:21 +0100

    Initial commit

---
+++ test/unit/lib/Generator.js
@@ -0,0 +1,42 @@
+//imports
+const path = require("path");
+const Dir = require("justo-fs").Dir;
+const file = require("justo-assert-fs").file;
+const dir = require("justo-assert-fs").dir;
+const Generator = require("../../../dist/es5/nodejs/justo-generator-justo");
+const suite = require("justo").suite;
+const test = require("justo").test;
+const init = require("justo").init;
+const fin = require("justo").fin;
+
+//suite
+suite("Generator", function() {
+  suite("#constructor()", function() {
+    test("constructor()", function() {
+      var gen = new Generator({});
+      gen.must.be.instanceOf(Generator);
+    });
+  });
+
+  suite("#generate()", function() {
+    var gen, DST_DIR, DST;
+
+    init("*", function() {
+      DST_DIR = Dir.createTmpDir();
+      DST = DST_DIR.path;
+      gen = new Generator({src: "dist/es5/nodejs/justo-generator-justo/template", dst: DST}, {});
+    });
+
+    fin("*", function() {
+      DST_DIR.remove();
+    });
+
+    test("generate(answers)", function() {
+      gen.generate({desc: "This is the description", author: "This is the author name"});
+
+      file(DST, "package.json").must.exist();
+      file(DST, "Justo.js").must.exist();
+      file(DST, "Justo.json").must.exist();
+    });
+  });
+})();
